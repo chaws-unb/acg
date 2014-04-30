87fa7414fb6173efdd8220fb8c75402a35e3dc26
Gabriela Navarro <navarro1703@gmail.com>
2014-04-30 00:11:38 -0300

    Transposition cipher, decipher and keyColumn generator.

--- transpositionCipher.c
+++ transpositionCipher.c
@@ -3,34 +3,54 @@
 #include <stdio.h>
 #include <math.h>
 
+int * keyColumnGenerator(int key){
+	static int r[8];
+	int i = 0;
+	int j = 0;
+	int value = 0;
+	for(; i < BLOCK_SIZE; i++){
+		value = abs(key % BLOCK_SIZE);
+		key = key >> 4;
+		int ok;
+		do {
+			ok = 1;
+			j = 0;
+			for(;j<i; j++){
+				if(r[j] == value){
+					value += 1 % BLOCK_SIZE;
+					ok = 0;
+					break;
+				}
+			}
+		} while (!ok);
+		r[i] = value;
+	}
+
+	return r;
+}
+
 char * transpositionCipher(const char * plainText, size_t length, int key)
 {
 	if(!plainText)
 		return NULL;
 
-	char * cipheredText = (char *)malloc(sizeof(char) * length);
+	int tam = length + length % BLOCK_SIZE + 1;
+	char * cipheredText = (char *)malloc(sizeof(char) * tam);
+	cipheredText[tam - 1] = '\0';
 	int rows = ((int)length / BLOCK_SIZE) + (((int)length % BLOCK_SIZE) ? 1 : 0);
 	char * text = (char *)malloc(rows*BLOCK_SIZE);
-	//Idéia é transformar a chave em um vetor do estilo:
-	int keyColumn[BLOCK_SIZE] = {4,2,0,7,5,1,6,3};
+	int *keyColumn;
+
+	keyColumn = keyColumnGenerator(key);
+	int keyIndex = 0;
+
 	int i = 0,j = 0, pi = 0;
-	for(;i < rows; i++){
-		for(;j < BLOCK_SIZE; j++){
-			text[i * BLOCK_SIZE + j] = plainText[pi] == '\0' ? ' ' : plainText[pi++];
-			printf("%c", text[i * BLOCK_SIZE + j]);
-			if( j == 7){
-				printf("\n");
-			}
+	for(;i < BLOCK_SIZE; i++){
+		for(;j < rows; j++){
+			pi = j * BLOCK_SIZE + *(keyColumn + i);
+			cipheredText[i * rows + j] = pi < length ? plainText[pi] : '!';
 		}
 	 j = 0;
-	}
-
-	int indexI = 0, indexJ = 0, ci = 0;
-	for(; indexI < BLOCK_SIZE ; indexI++){
-		for(;indexJ < rows ; indexJ++){
-			cipheredText[ci++] = text[indexJ * BLOCK_SIZE + keyColumn[indexI]];
-		}
-		indexJ = 0;
 	}
 
 	return cipheredText;
@@ -41,37 +61,30 @@ char * transpositionDecipher(const char * cipheredText, size_t length, int key)
 	if(!cipheredText)
 		return NULL;
 
-	char * plainText = (char *)malloc(sizeof(char) * length);
+	char * plainText = (char *)malloc(sizeof(char) * length + 1);
+	plainText[length] = '\0';
 	int rows = ((int)length / BLOCK_SIZE) + (((int)length % BLOCK_SIZE) ? 1 : 0);
 	char * text = (char *)malloc(rows*BLOCK_SIZE);
-	//Idéia é transformar a chave em um vetor do estilo:
-	int keyColumn[BLOCK_SIZE] = {4,2,0,7,5,1,6,3};
+	int *keyColumn;
+
+	keyColumn = keyColumnGenerator(key);
 	int i = 0,j = 0, pi = 0;
-	printf("----------------------DECIPHER--------------------\n");
 	for(;i < BLOCK_SIZE; i++){
 		for(;j < rows; j++){
-			text[j * BLOCK_SIZE + i] = cipheredText[pi] == '\0' ? ' ' : cipheredText[pi++];
+			pi = j * BLOCK_SIZE + *(keyColumn + i);
+			if( pi < length){
+				plainText[pi] = cipheredText[i * rows + j];
+			}
+			
 		}
 	 j = 0;
 	}
-	int k = 0, l=0;
-	for(; k < rows; k++){
-		for(;l< BLOCK_SIZE; l++){
-			printf("%c", text[k*BLOCK_SIZE+l]);
-			if(l == 7){
-				printf("\n");
-			}
-		}
-		l=0;
-	}
-
-	int indexI = 0, indexJ = 0, ci = 0;
-	for(; indexI < rows ; indexI++){
-		for(;indexJ < BLOCK_SIZE ; indexJ++){
-			plainText[ci++] = text[indexI * BLOCK_SIZE + keyColumn[indexJ]];
-		}
-		indexJ = 0;
-	}
+	// for (i=length-BLOCK_SIZE; i<length; i++) {
+	// 	printf("I: %i, C: %c\n",i, plainText[i] );
+	// 	if (plainText[i] == '!') {
+	// 		plainText[i] = '\0';
+	// 	}
+	// }
 
 	return plainText;
 }
